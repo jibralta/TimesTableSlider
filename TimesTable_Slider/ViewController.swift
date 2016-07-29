26eeba29e576b4be1501bf1e182d0e617ea062ec
jibralta <joyumali@gmail.com>
2016-07-29 01:44:40 -0700

    Connect slider & table view.

--- TimesTable_Slider/ViewController.swift
+++ TimesTable_Slider/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
 
+    @IBOutlet var slider: UISlider!
+    
+    @IBAction func moveSlider(_ sender: AnyObject) {
+        
+        print(slider.value)
+        
+        // table.reloadData()
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 50
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test Return"
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
